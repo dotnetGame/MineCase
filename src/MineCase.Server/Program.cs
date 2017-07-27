cbbb3fd98ecdb5cc54f0cf5b8e817fd779397551
SunnyCase <sunnycase@live.cn>
2017-07-27 15:01:13 +0800

    初次提交

---
+++ src/MineCase.Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MineCase.Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
