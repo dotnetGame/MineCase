45f7b2fd161468ebefb548de2114dbd3e1b176ef
WangJun <1103870790@qq.com>
2017-08-23 17:36:00 +0800

    Fix a mistake in IServerSettings

--- src/MineCase.Server.Interfaces/Settings/IServerSettings.cs
+++ src/MineCase.Server.Interfaces/Settings/IServerSettings.cs
@@ -1,8 +1,14 @@
-﻿using Orleans;
+﻿using System.Threading.Tasks;
+using Orleans;
 
 namespace MineCase.Server.Settings
 {
     public interface IServerSettings : IGrainWithIntegerKey
     {
+        // get settings
+        Task<ServerSettings> GetSettigns();
+
+        // set settings
+        Task SetSettigns(ServerSettings settings);
     }
 }
