e7abc50a323120a92594cd44de41db7696c49105
Alinshans <13925588730@163.com>
2017-08-24 23:07:36 +0800

    Update tests.

--- tests/UnitTest/ChatTest.cs
+++ tests/UnitTest/ChatTest.cs
@@ -21,9 +21,8 @@ namespace MineCase.UnitTest
             stringComponent.Text = "hello case!";
             stringComponent.Bold = true;
             stringComponent.Itatic = false;
-            stringComponent.Color = "blue";
+            stringComponent.Color = ColorType.Blue;
             stringComponent.ClickEvent = new ChatClickEvent(ClickEventType.OpenUrl, @"http://case.orz");
-            stringComponent.Extra = new JArray(JObject.Parse(@"{""text"":""foo""}"), JObject.Parse(@"{""text"":""bar""}"));
             chat.Component = stringComponent;
 
             JObject o = chat.ToJObject();
@@ -33,8 +32,6 @@ namespace MineCase.UnitTest
             Assert.Equal("blue", o.GetValue("color").Value<string>());
             Assert.Equal("open_url", (string)o.SelectToken("clickEvent.action"));
             Assert.Equal(@"http://case.orz", (string)o.SelectToken("clickEvent.value"));
-            Assert.Equal("foo", (string)o.SelectToken("extra[0].text"));
-            Assert.Equal("bar", (string)o.SelectToken("extra[1].text"));
             Assert.True(JToken.DeepEquals(o, o2));
         }
 
@@ -48,14 +45,14 @@ namespace MineCase.UnitTest
             stringComponent.Text = "hello";
             stringComponent.Bold = true;
             stringComponent.Itatic = false;
-            stringComponent.Color = "green";
+            stringComponent.Color = ColorType.Green;
             ChatClickEvent chatClickEvent = new ChatClickEvent();
             chatClickEvent.Action = ClickEventType.ChangePage;
             chatClickEvent.Value = 1;
             stringComponent.ClickEvent = chatClickEvent;
 
             Chat chat = new Chat(stringComponent);
-            Assert.Equal("{\"bold\":true,\"color\":\"green\",\"clickEvent\":{\"action\":\"change_page\",\"value\":1},\"text\":\"hello\"}", chat.ToString());
+            Assert.Equal("{\"bold\":true,\"itatic\":false,\"color\":\"green\",\"clickEvent\":{\"action\":\"change_page\",\"value\":1},\"text\":\"hello\"}", chat.ToString());
         }
 
         /// <summary>
@@ -74,7 +71,7 @@ namespace MineCase.UnitTest
                     ""value"":1
                 },
                 ""extra"":[
-                    { ""text"":""foo"" },
+                    { ""text"":""foo"",""bold"":false },
                     { ""text"":""bar"" }
                 ]
             }";
@@ -89,11 +86,88 @@ namespace MineCase.UnitTest
             Assert.Equal("case", sc.Text);
             Assert.True(sc.Bold);
             Assert.True(sc.Itatic);
-            Assert.Equal("red", sc.Color);
+            Assert.Equal(ColorType.Red, sc.Color);
             Assert.Equal(ClickEventType.ChangePage, sc.ClickEvent.Action);
             Assert.Equal(1, sc.ClickEvent.Value.Value<int>());
+            Assert.False(jObject.SelectToken("extra[0].bold").Value<bool>());
             Assert.Equal("foo", jObject.SelectToken("extra[0].text"));
             Assert.Equal("bar", jObject.SelectToken("extra[1].text"));
+        }
+
+        /// <summary>
+        /// More test cases.
+        /// </summary>
+        [Fact]
+        public void Test4()
+        {
+            Chat chat = new Chat("text");
+            chat.Component.Bold = true;
+            chat.Component.Itatic = false;
+            chat.Component.Insertion = "insert";
+            chat.Component.Color = ColorType.Red;
+            chat.Component.ClickEvent = new ChatClickEvent(ClickEventType.ChangePage, 1);
+            chat.Component.HoverEvent = new ChatHoverEvent(HoverEventType.ShowText, "show");
+            KeybindComponent keybind = new KeybindComponent();
+            keybind.Bold = false;
+            keybind.Itatic = true;
+            keybind.Color = ColorType.Green;
+            chat.Component.Extra = new List<ChatComponent>() { keybind };
+
+            var json = chat.ToJObject();
+            Assert.Null(json.SelectToken("underlined"));
+            Assert.Null(json.SelectToken("strikethrough"));
+            Assert.Null(json.SelectToken("obfuscated"));
+            Assert.True(json.SelectToken("bold").Value<bool>());
+            Assert.False(json.SelectToken("extra[0].bold").Value<bool>());
+            Assert.False(json.SelectToken("itatic").Value<bool>());
+            Assert.True(json.SelectToken("extra[0].itatic").Value<bool>());
+            Assert.Equal("red", json.SelectToken("color"));
+            Assert.Equal("green", json.SelectToken("extra[0].color"));
+            Assert.Equal("insert", json.SelectToken("insertion"));
+            Assert.Equal("change_page", json.SelectToken("clickEvent.action"));
+            Assert.True((int)json.SelectToken("clickEvent.value") == 1);
+            Assert.Equal("show_text", json.SelectToken("hoverEvent.action"));
+            Assert.Equal("show", json.SelectToken("hoverEvent.value"));
+        }
+
+        /// <summary>
+        /// Tests TranslationComponent.
+        /// </summary>
+        [Fact]
+        public void Test5()
+        {
+            StringComponent sc = new StringComponent("text");
+            sc.Bold = true;
+            sc.ClickEvent = new ChatClickEvent(ClickEventType.RunCommand, "/msg a");
+            List<ChatComponent> list = new List<ChatComponent>();
+            list.Add(sc);
+            list.Add(new StringComponent("nothing"));
+
+            Chat chat = new Chat(new TranslationComponent("chat.type.text", list));
+            var jObject = chat.ToJObject();
+            Assert.Equal("chat.type.text", jObject.SelectToken("translate"));
+            Assert.True(jObject.SelectToken("with[0].bold").Value<bool>());
+            Assert.Equal("run_command", jObject.SelectToken("with[0].clickEvent.action"));
+            Assert.Equal("/msg a", jObject.SelectToken("with[0].clickEvent.value"));
+            Assert.Equal("text", jObject.SelectToken("with[0].text"));
+            Assert.Equal("nothing", jObject.SelectToken("with[1].text"));
+
+            string json = @"{
+                ""translate"":""chat.type.text"",
+                ""with"":[
+                    {
+                        ""bold"":true,
+                        ""text"":""text"",
+                        ""clickEvent"":{""action"":""run_command"",""value"":""/msg a""}
+                    },
+                    {
+                        ""text"":""nothing"" 
+                    }
+                ]
+            }";
+            var chat2 = Chat.Parse(json);
+            var jObject2 = chat2.ToJObject();
+            Assert.True(JToken.DeepEquals(jObject, jObject2));
         }
     }
 }
