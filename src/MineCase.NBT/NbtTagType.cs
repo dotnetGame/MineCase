58732b0f88f2aa3a94f18b6e9b1d6dad05e898b9
akemimadoka <homuhomu@honoka.moe>
2017-08-24 17:53:47 +0800

    Add nbt support (#9)
    * 创建 develop-NBT 分支
    
    * 添加readme (#2)
    
    * 添加readme
    
    * 添加在client非1.12版本下,LoginDisconnect提示
    
    * :pencil: Update README.
    
    * :pencil: Update README.
    
    * :pencil: Update README.
    
    * :arrow_forward: Add bat file.
    
    * :arrow_forward: Add bat file.
    
    * :pencil: Fix typo.
    
    * :pencil: Update README.
    
    * Add new line
    
    * Remove useless comments
    
    * 'ver' to 'version'
    
    * Correct the type of reason in LoginDisconnect.
    
    * ver -> version
    
    * Add StyleCop (#5)
    
    * Add StyleCop
    
    * Add StyleCop for Grains
    
    * Fix errors
    
    * 创建 develop-NBT 分支
    
    * 补上缺失的接口，修复编译失败
    
    * 实现 NbtTag 的构造方法，实现部分序列化及反序列化接口
    
    * 实现所有 Tag 的序列化及反序列化
    
    * 移除了错误的更改
    
    * 移除错误的更改

---
+++ src/MineCase.NBT/NbtTagType.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Nbt
+{
+    /// <summary>
+    /// 表示 <see cref="NbtTagType"/> 所关联的 <see cref="Tags.NbtTag"/> 类型
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Field)]
+    internal class TagClassAttribute : Attribute
+    {
+        internal Type TagClassType { get; }
+
+        internal TagClassAttribute(Type type)
+        {
+            TagClassType = type;
+        }
+    }
+
+    /// <summary>
+    /// NBT Tag 的类型
+    /// </summary>
+    /// <remarks>注释内容来自<see href="http://wiki.vg/NBT#Specification"/></remarks>
+    public enum NbtTagType : byte
+    {
+        /// <summary>
+        /// Signifies the end of a Compound (<see cref="Compound"/>). It is only ever used inside a Compound, and is not named despite being in a Compound
+        /// </summary>
+        [TagClass(typeof(Tags.NbtEnd))]
+        End = 0x00,
+
+        /// <summary>
+        /// A single signed byte (<see cref="sbyte"/>)
+        /// </summary>
+        [TagClass(typeof(Tags.NbtByte))]
+        Byte = 0x01,
+
+        /// <summary>
+        /// A single signed, big endian 16 bit integer (<see cref="short"/>)
+        /// </summary>
+        [TagClass(typeof(Tags.NbtShort))]
+        Short = 0x02,
+
+        /// <summary>
+        /// A single signed, big endian 32 bit integer (<see cref="int"/>)
+        /// </summary>
+        [TagClass(typeof(Tags.NbtInt))]
+        Int = 0x03,
+
+        /// <summary>
+        /// A single signed, big endian 64 bit integer (<see cref="long"/>)
+        /// </summary>
+        [TagClass(typeof(Tags.NbtLong))]
+        Long = 0x04,
+
+        /// <summary>
+        /// A single, big endian IEEE-754 single-precision floating point number (<see cref="float"/>)
+        /// </summary>
+        [TagClass(typeof(Tags.NbtFloat))]
+        Float = 0x05,
+
+        /// <summary>
+        /// A single, big endian IEEE-754 double-precision floating point number (<see cref="double"/>)
+        /// </summary>
+        [TagClass(typeof(Tags.NbtDouble))]
+        Double = 0x06,
+
+        /// <summary>
+        /// A length-prefixed array of signed bytes (<see cref="T:sbyte[]"/>). The prefix is a signed integer (thus 4 bytes, <see cref="int"/>)
+        /// </summary>
+        [TagClass(typeof(Tags.NbtByteArray))]
+        ByteArray = 0x07,
+
+        /// <summary>
+        /// A length-prefixed UTF-8 string (<see cref="string"/>).
+        /// The prefix is an unsigned short (thus 2 bytes, <see cref="ushort"/>) signifying the length of the string in bytes
+        /// </summary>
+        [TagClass(typeof(Tags.NbtString))]
+        String = 0x08,
+
+        /// <summary>
+        /// A list of nameless tags, all of the same type.
+        /// The list is prefixed with the Type ID of the items it contains (thus 1 byte), and the length of the list as a signed integer (a further 4 bytes).
+        /// If the length of the list is 0 or negative, the type may be 0 (<see cref="End"/>) but otherwise it must be any other type.
+        /// </summary>
+        /// <remarks>
+        /// The notchian implementation uses End (<see cref="End"/>) in that situation, but another reference implementation by Mojang uses 1 instead;
+        /// parsers should accept any type if the length is &lt;= 0
+        /// </remarks>
+        [TagClass(typeof(Tags.NbtList))]
+        List = 0x09,
+
+        /// <summary>
+        /// Effectively a list of a named tags. Order is not guaranteed.
+        /// </summary>
+        [TagClass(typeof(Tags.NbtCompound))]
+        Compound = 0x0a,
+
+        /// <summary>
+        /// A length-prefixed array of signed integers (<see cref="T:int[]"/>).
+        /// The prefix is a signed integer (thus 4 bytes, <see cref="int"/>) and indicates the number of 4 byte integers.
+        /// </summary>
+        [TagClass(typeof(Tags.NbtIntArray))]
+        IntArray = 0x0b
+    }
+}
---
+++ src/MineCase.NBT/Serialization/NbtTagSerializer.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using MineCase.Nbt.Tags;
+
+namespace MineCase.Nbt.Serialization
+{
+    /// <summary>
+    /// Tag 的自定义序列化及反序列化接口
+    /// </summary>
+    internal interface ITagSerializer
+    {
+        /// <summary>
+        /// 反序列化
+        /// </summary>
+        /// <param name="br">已打开的 <see cref="BinaryReader"/></param>
+        /// <param name="requireName">当前上下文是否需要 Tag 具有名称</param>
+        /// <remarks>实现保证此方法被调用之时一定已经读取到了当前注册到的 <see cref="NbtTagType"/>，且 <paramref name="br"/> 一定不为 null</remarks>
+        NbtTag Deserialize(BinaryReader br, bool requireName);
+
+        /// <summary>
+        /// 序列化
+        /// </summary>
+        /// <param name="tag">要序列化的 Tag</param>
+        /// <param name="bw">已打开的 <see cref="BinaryWriter"/></param>
+        /// <remarks>
+        /// 实现保证此方法被调用之时一定已经写入了当前注册到的 <see cref="NbtTagType"/>，
+        /// 且 <paramref name="tag"/> 及 <paramref name="bw"/> 一定不为 null，
+        /// 并且 <paramref name="tag"/> 一定为当前注册到的 <see cref="NbtTagType"/> 关联的 <see cref="NbtTag"/> 类型
+        /// </remarks>
+        void Serialize(NbtTag tag, BinaryWriter bw);
+    }
+
+    /// <summary>
+    /// 用于将 NbtTag 进行序列化和反序列化的类
+    /// </summary>
+    public static class NbtTagSerializer
+    {
+        private static readonly Dictionary<NbtTagType, ITagSerializer> TagDictionary = new Dictionary<NbtTagType, ITagSerializer>();
+
+        internal static void RegisterTag(NbtTagType tagType, ITagSerializer tagSerializer)
+        {
+            TagDictionary.Add(tagType, tagSerializer);
+        }
+
+        /// <summary>
+        /// 反序列化 Tag
+        /// </summary>
+        /// <param name="br">已打开的 <see cref="BinaryReader"/></param>
+        /// <param name="requireName">当前上下文是否需要 Tag 具有名称</param>
+        /// <exception cref="ArgumentNullException"><paramref name="br"/> 为 null</exception>
+        public static NbtTag DeserializeTag(BinaryReader br, bool requireName)
+        {
+            if (br == null)
+            {
+                throw new ArgumentNullException(nameof(br));
+            }
+
+            Contract.EndContractBlock();
+
+            var tagType = br.ReadTagType();
+            return TagDictionary[tagType].Deserialize(br, requireName);
+        }
+
+        /// <summary>
+        /// 以指定的类型反序列化 Tag
+        /// </summary>
+        /// <typeparam name="T">指定的类型</typeparam>
+        /// <param name="br">已打开的 <see cref="BinaryReader"/></param>
+        /// <param name="requireName">当前上下文是否需要 Tag 具有名称</param>
+        /// <exception cref="ArgumentNullException"><paramref name="br"/> 为 null</exception>
+        /// <exception cref="InvalidCastException">无法将获得的 Tag 转换到类型 <typeparamref name="T"/></exception>
+        public static T DeserializeTag<T>(BinaryReader br, bool requireName)
+            where T : NbtTag
+        {
+            return (T)DeserializeTag(br, requireName);
+        }
+
+        /// <summary>
+        /// 使用已知的 <see cref="NbtTagType"/> 反序列化 Tag
+        /// </summary>
+        /// <param name="br">已打开的 <see cref="BinaryReader"/></param>
+        /// <param name="tagType">已知的 <see cref="NbtTagType"/></param>
+        /// <param name="requireName">当前上下文是否需要 Tag 具有名称</param>
+        /// <exception cref="ArgumentNullException"><paramref name="br"/> 为 null</exception>
+        public static NbtTag DeserializeTag(BinaryReader br, NbtTagType tagType, bool requireName)
+        {
+            if (br == null)
+            {
+                throw new ArgumentNullException(nameof(br));
+            }
+
+            Contract.EndContractBlock();
+
+            return TagDictionary[tagType].Deserialize(br, requireName);
+        }
+
+        /// <summary>
+        /// 使用已知的 <see cref="NbtTagType"/>，以指定的类型反序列化 Tag
+        /// </summary>
+        /// <typeparam name="T">指定的类型</typeparam>
+        /// <param name="br">已打开的 <see cref="BinaryReader"/></param>
+        /// <param name="tagType">已知的 <see cref="NbtTagType"/></param>
+        /// <param name="requireName">当前上下文是否需要 Tag 具有名称</param>
+        /// <exception cref="ArgumentNullException"><paramref name="br"/> 为 null</exception>
+        /// <exception cref="InvalidCastException">无法将获得的 Tag 转换到类型 <typeparamref name="T"/></exception>
+        public static T DeserializeTag<T>(BinaryReader br, NbtTagType tagType, bool requireName)
+            where T : NbtTag
+        {
+            return (T)DeserializeTag(br, tagType, requireName);
+        }
+
+        /// <summary>
+        /// 序列化 Tag
+        /// </summary>
+        /// <param name="tag">要序列化的 Tag</param>
+        /// <param name="bw">已打开的 <see cref="BinaryWriter"/></param>
+        /// <param name="writeTagType">指示是否需要写入 <see cref="NbtTagType"/></param>
+        /// <exception cref="ArgumentNullException"><paramref name="tag"/> 或 <paramref name="bw"/> 为 null</exception>
+        public static void SerializeTag(NbtTag tag, BinaryWriter bw, bool writeTagType = true)
+        {
+            if (tag == null)
+            {
+                throw new ArgumentNullException(nameof(tag));
+            }
+
+            if (bw == null)
+            {
+                throw new ArgumentNullException(nameof(bw));
+            }
+
+            Contract.EndContractBlock();
+
+            var tagType = tag.TagType;
+            var tagSerializer = TagDictionary[tagType];
+
+            if (writeTagType)
+            {
+                bw.WriteTagValue(tagType);
+            }
+
+            tagSerializer.Serialize(tag, bw);
+        }
+    }
+}
