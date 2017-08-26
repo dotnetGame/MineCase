e575bd28237146677d4ae078a3af6d4b0b0a08b3
Guo Hui <sunnycase@live.cn>
2017-08-26 16:27:01 +0800

    添加 Noise 基架

---
+++ src/MineCase.Algorithm/Noise/PerlinNoise.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Algorithm.Noise
+{
+    public class PerlinNoise
+    {
+        private readonly int[] _permutation = new int[512];
+
+        public PerlinNoise(int seed)
+        {
+            var random = new Random(seed);
+            for (int i = 0; i < 256; i++)
+                _permutation[i + 256] = _permutation[i] = random.Next(0, 256);
+        }
+
+        public double Noise(double x, double y, double z)
+        {
+            return 255;
+        }
+    }
+}
---
+++ tests/UnitTest/NoiseTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using ImageSharp;
+using MineCase.Algorithm.Noise;
+using Xunit;
+
+namespace MineCase.UnitTest
+{
+    public class NoiseTest
+    {
+        public static readonly string RootDir = @"F:\Work\Repository\MineCase\tests\UnitTest\bin\Debug";
+
+        [Fact]
+        public void TestPerlinNoise3D()
+        {
+            const int xExtent = 100;
+            const int yExtent = 100;
+
+            using (var file = File.OpenWrite(Path.Combine(RootDir, "PerlinNoise3D.bmp")))
+            using (var image = new Image<ImageSharp.PixelFormats.Rgb24>(xExtent, yExtent))
+            {
+                var noise = new PerlinNoise();
+                for (int x = 0; x < xExtent; x++)
+                {
+                    for (int y = 0; y < yExtent; y++)
+                    {
+                        var color = (byte)noise.Noise(x, 0, y);
+                        image[x, y] = new ImageSharp.PixelFormats.Rgb24(color, color, color);
+                    }
+                }
+
+                image.SaveAsBmp(file);
+            }
+        }
+    }
+}
