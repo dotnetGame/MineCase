029104c030c3cf682a5b1a472ac202c89d05cab0
melonpi <melonpi1943@gmail.com>
2017-08-30 15:01:49 +0800

    modify bat files.

--- run_server.bat
+++ run_server.bat
@@ -1,8 +1,13 @@
-cd src
+@echo off
+
+echo building MineCase...
+pushd src
 dotnet restore
-dotnet build
+dotnet build -c debug
+popd
 
-cd MineCase.Server
+echo start MineCase.Server...
+pushd src\MineCase.Server
 start dotnet run
-
-pause
+echo run "run_gateway.bat" when server is ready.
+popd
