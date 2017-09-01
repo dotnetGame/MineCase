5218f70f5132dceaa08e42b01ad27ef7b1a9edf3
WangJun <1103870790@qq.com>
2017-09-02 02:31:17 +0800

    Add more methods

--- src/MineCase.Server.Interfaces/World/Position.cs
+++ src/MineCase.Server.Interfaces/World/Position.cs
@@ -33,35 +33,6 @@ namespace MineCase.Server.World
         }
     }
 
-    public struct Vector3f
-    {
-        public float X { get; set; }
-
-        public float Y { get; set; }
-
-        public float Z { get; set; }
-
-        public Vector3f(float x, float y, float z)
-        {
-            X = x;
-            Y = y;
-            Z = z;
-        }
-    }
-
-    public struct Vector2f
-    {
-        public float X { get; set; }
-
-        public float Z { get; set; }
-
-        public Vector2f(float x, float z)
-        {
-            X = x;
-            Z = z;
-        }
-    }
-
     public struct BlockWorldPos
     {
         public int X { get; set; }
@@ -94,6 +65,36 @@ namespace MineCase.Server.World
             if (chunkPosZ < 0) chunkPosZ -= 1;
             return new ChunkWorldPos(chunkPosX, chunkPosZ);
         }
+
+        public static BlockWorldPos Add(BlockWorldPos pos, int x, int y, int z)
+        {
+            return new BlockWorldPos(pos.X + x, pos.Y + y, pos.Z + z);
+        }
+
+        public static int Clamp(int value, int max, int min)
+        {
+            int result;
+            if (value > max) result = max;
+            else if (value < min) result = min;
+            else result = value;
+            return result;
+        }
+
+        public static BlockWorldPos Clamp(BlockWorldPos value, BlockWorldPos max, BlockWorldPos min)
+        {
+            return new BlockWorldPos(
+                Clamp(value.X, max.X, min.X),
+                Clamp(value.Y, max.Y, min.Y),
+                Clamp(value.Z, max.Z, min.Z));
+        }
+
+        public static double Distance(BlockWorldPos pos1, BlockWorldPos pos2)
+        {
+            int x = pos1.X - pos2.X;
+            int y = pos1.Y - pos2.Y;
+            int z = pos1.Z - pos2.Z;
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
     }
 
     public struct BlockChunkPos
@@ -117,6 +118,36 @@ namespace MineCase.Server.World
             int blockY = Y;
             int blockZ = Z + chunkPos.Z * 16;
             return new BlockWorldPos(blockX, blockY, blockZ);
+        }
+
+        public static BlockChunkPos Add(BlockChunkPos pos, int x, int y, int z)
+        {
+            return new BlockChunkPos(pos.X + x, pos.Y + y, pos.Z + z);
+        }
+
+        public static int Clamp(int value, int max, int min)
+        {
+            int result;
+            if (value > max) result = max;
+            else if (value < min) result = min;
+            else result = value;
+            return result;
+        }
+
+        public static BlockChunkPos Clamp(BlockChunkPos value, BlockChunkPos max, BlockChunkPos min)
+        {
+            return new BlockChunkPos(
+                Clamp(value.X, max.X, min.X),
+                Clamp(value.Y, max.Y, min.Y),
+                Clamp(value.Z, max.Z, min.Z));
+        }
+
+        public static double Distance(BlockChunkPos pos1, BlockChunkPos pos2)
+        {
+            int x = pos1.X - pos2.X;
+            int y = pos1.Y - pos2.Y;
+            int z = pos1.Z - pos2.Z;
+            return Math.Sqrt(x * x + y * y + z * z);
         }
     }
 
@@ -168,6 +199,52 @@ namespace MineCase.Server.World
             Y = y;
             Z = z;
         }
+
+        public ChunkWorldPos ToChunkWorldPos()
+        {
+            float chunkX = (float)Math.Floor((double)X / ChunkConstants.BlockEdgeWidthInSection);
+            float chunkZ = (float)Math.Floor((double)Z / ChunkConstants.BlockEdgeWidthInSection);
+            return new ChunkWorldPos((int)chunkX, (int)chunkZ);
+        }
+
+        public EntityChunkPos ToEntityChunkPos()
+        {
+            float entityX = X % ChunkConstants.BlockEdgeWidthInSection;
+            float entityZ = Z % ChunkConstants.BlockEdgeWidthInSection;
+            if (entityX < 0) entityX += ChunkConstants.BlockEdgeWidthInSection;
+            if (entityZ < 0) entityZ += ChunkConstants.BlockEdgeWidthInSection;
+            return new EntityChunkPos(entityX, Y, entityZ);
+        }
+
+        public static EntityWorldPos Add(EntityWorldPos pos, float x, float y, float z)
+        {
+            return new EntityWorldPos(pos.X + x, pos.Y + y, pos.Z + z);
+        }
+
+        public static float Clamp(float value, float max, float min)
+        {
+            float result;
+            if (value > max) result = max;
+            else if (value < min) result = min;
+            else result = value;
+            return result;
+        }
+
+        public static EntityWorldPos Clamp(EntityWorldPos value, EntityWorldPos max, EntityWorldPos min)
+        {
+            return new EntityWorldPos(
+                Clamp(value.X, max.X, min.X),
+                Clamp(value.Y, max.Y, min.Y),
+                Clamp(value.Z, max.Z, min.Z));
+        }
+
+        public static double Distance(EntityWorldPos pos1, EntityWorldPos pos2)
+        {
+            float x = pos1.X - pos2.X;
+            float y = pos1.Y - pos2.Y;
+            float z = pos1.Z - pos2.Z;
+            return Math.Sqrt(x * x + y * y + z * z);
+        }
     }
 
     public struct EntityChunkPos
@@ -183,6 +260,41 @@ namespace MineCase.Server.World
             X = x;
             Y = y;
             Z = z;
+        }
+
+        public EntityWorldPos ToEntityWorldPos(ChunkWorldPos pos)
+        {
+            return new EntityWorldPos(X + pos.X, Y, Z + pos.Z);
+        }
+
+        public static EntityChunkPos Add(EntityChunkPos pos, float x, float y, float z)
+        {
+            return new EntityChunkPos(pos.X + x, pos.Y + y, pos.Z + z);
+        }
+
+        public static float Clamp(float value, float max, float min)
+        {
+            float result;
+            if (value > max) result = max;
+            else if (value < min) result = min;
+            else result = value;
+            return result;
+        }
+
+        public static EntityChunkPos Clamp(EntityChunkPos value, EntityChunkPos max, EntityChunkPos min)
+        {
+            return new EntityChunkPos(
+                Clamp(value.X, max.X, min.X),
+                Clamp(value.Y, max.Y, min.Y),
+                Clamp(value.Z, max.Z, min.Z));
+        }
+
+        public static double Distance(EntityChunkPos pos1, EntityChunkPos pos2)
+        {
+            float x = pos1.X - pos2.X;
+            float y = pos1.Y - pos2.Y;
+            float z = pos1.Z - pos2.Z;
+            return Math.Sqrt(x * x + y * y + z * z);
         }
     }
 }
