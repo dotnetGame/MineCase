48503cc52c5eb46639dabadf06ecbcd5f2fbf49d
Guo Hui <sunnycase@live.cn>
2017-09-02 16:35:12 +0800

    hmmm

---
+++ src/MineCase.Server.Grains/Game/Windows/SlotAreas/CraftingSlotArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Server.Game.Windows.SlotAreas
+{
+    internal class CraftingSlotArea : SlotArea
+    {
+        public CraftingSlotArea()
+        {
+        }
+    }
+}
