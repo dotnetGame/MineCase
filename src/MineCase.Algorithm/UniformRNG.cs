debc078ab1799dc531e07921c6acefbf7b021b56
Alinshans <13925588730@163.com>
2017-09-05 16:18:56 +0800

    Add RNG.

---
+++ src/MineCase.Algorithm/UniformRNG.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Algorithm
+{
+    public struct UniformRNG
+    {
+        private UInt64 _state;
+
+        public UniformRNG(UInt64 state)
+        {
+            _state = state == 0 ? 0xFFFFFFFF : state;
+        }
+
+        public int NextInt()
+        {
+            return (int)Next();
+        }
+
+        public uint NextUInt()
+        {
+            return Next();
+        }
+
+        public float NextFloat()
+        {
+            return Next() * 2.3283064365386962890625e-10f;
+        }
+
+        public double NextDouble()
+        {
+            var t = Next();
+            return (((UInt64)t << 32) | Next()) * 5.4210108624275221700372640043497e-20;
+        }
+
+        public int Uniform(int a, int b)
+        {
+            return a == b ? a : (int)(Next() % (b - a) + a);
+        }
+
+        public uint Uniform(uint a, uint b)
+        {
+            return a == b ? a : Next() % (b - a) + a;
+        }
+
+        public float Uniform(float a, float b)
+        {
+           return _state * (b - a) + a;
+        }
+
+        public double Uniform(double a, double b)
+        {
+            return _state * (b - a) + a;
+        }
+
+        private uint Next()
+        {
+            // _state ^= _state >> 13;
+            // _state ^= (_state << 7) | 0x9d2c5680;
+            // _state ^= (_state << 15) & 0xefc60000;
+            // _state = (_state << 32) | (_state >> 17) ^ 0xa3d40a0b;
+            _state = (UInt64)(uint)_state * 4164903690 + (uint)(_state >> 32);
+            return (uint)_state;
+        }
+    }
+}
---
+++ tests/UnitTest/RNGTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text;
+using ImageSharp;
+using MineCase.Algorithm;
+using Xunit;
+
+namespace MineCase.UnitTest
+{
+    public class RNGTest
+    {
+        public readonly string RootDir;
+
+        public RNGTest()
+        {
+            RootDir = SetRootDir();
+        }
+
+        private static string SetRootDir([CallerFilePath]string fileName = null) =>
+            Path.Combine(Path.GetDirectoryName(fileName), "bin");
+
+        [Fact]
+        public void TestNext()
+        {
+            UniformRNG rng = new UniformRNG(0x123456789);
+            int first = rng.NextInt(), second = rng.NextInt();
+            for (int i = 0; i < 1000000; ++i)
+            {
+                first = second;
+                second = rng.NextInt();
+                Assert.NotEqual(first, second);
+            }
+        }
+
+        [Fact]
+        public void TestUniform()
+        {
+            UniformRNG rng = new UniformRNG(0x123456789);
+            var i1 = rng.Uniform(1, 2);
+            Assert.Equal(1, i1);
+            uint a = 1, b = 2;
+            var i2 = rng.Uniform(a, b);
+            Assert.Equal((uint)1, i2);
+        }
+
+        [Fact]
+        public void TestRNGImg()
+        {
+            UniformRNG rng = new UniformRNG(0);
+            int[] bucket = new int[1000];
+
+            const int xExtent = 1024;
+            const int yExtent = 400;
+
+            using (var file = File.OpenWrite(Path.Combine(RootDir, "rng.bmp")))
+            using (var image = new Image<ImageSharp.PixelFormats.Rgb24>(xExtent, yExtent))
+            {
+                for (int i = 0; i < 100000; ++i)
+                {
+                    bucket[rng.Uniform(0, 1000)]++;
+                }
+
+                for (int i = 0; i < 1000; ++i)
+                {
+                    for (int j = 0; j < bucket[i]; ++j)
+                    {
+                        image[i, j] = new ImageSharp.PixelFormats.Rgb24((byte)0xFF, (byte)0x69, (byte)0xB4);
+                    }
+                }
+
+                image.SaveAsBmp(file);
+            }
+        }
+    }
+}
