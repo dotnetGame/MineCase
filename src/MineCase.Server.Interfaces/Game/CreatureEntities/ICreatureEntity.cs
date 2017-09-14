d8a8f756b96a4410f641c0df4feb6687e89d17c5
WangJun <1103870790@qq.com>
2017-09-14 16:53:11 +0800

    Add passive creature tick

---
+++ src/MineCase.Server.Interfaces/Game/CreatureEntities/ICreatureEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Orleans;
+
+namespace MineCase.Server.Game.CreatureEntities
+{
+    public interface ICreatureEntity : IGrainWithStringKey
+    {
+        Task OnCreated();
+
+        Task Destroy();
+    }
+}
---
+++ src/MineCase.Server.Interfaces/Game/CreatureEntities/IPassiveCreatureEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Server.Game.CreatureEntities
+{
+    public interface IPassiveCreatureEntity : ICreatureEntity, ITickable
+    {
+
+    }
+}
