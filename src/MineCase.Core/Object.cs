739fcff85c5d772613e8d30820ec5fb9e048cec8
Alinshans <13925588730@163.com>
2017-09-25 17:44:45 +0800

    Add object id.

---
+++ src/MineCase.Core/Object.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase
+{
+    {
+        Boat = 1,
+        ItemStack = 2,
+        AreaEffectCloud = 3,
+        Minecart = 10,
+        ActivatedTnt = 50,
+        Endercrystal = 51,
+        TippedArrow = 60,
+        Snowball = 61,
+        Egg = 62,
+        Fireball = 63,
+        Firecharge = 64,
+        ThrownEnderpearl = 65,
+        WitherSkull = 66,
+        ShulkerBullet = 67,
+        LlamaSpit = 68,
+        FallingObjects = 70,
+        ItemFrames = 71,
+        EyeOfEnder = 72,
+        ThrownPotion = 73,
+        ThrownExpBottle = 75,
+        FireworkRocket = 76,
+        LeashKnot = 77,
+        Armorstand = 78,
+        EvocationFangs = 79,
+        FishingHook = 90,
+        SpectralArrow = 91,
+        DragonFireball = 93
+    }
+}
