2ae9cf1738ac779df6723661b0aa8cee043ef239
WangJun <1103870790@qq.com>
2017-10-01 21:30:13 +0800

    Fix mistake

--- src/MineCase.Algorithm/World/Plants/TreeGenerator.cs
+++ src/MineCase.Algorithm/World/Plants/TreeGenerator.cs
@@ -220,6 +220,7 @@ namespace MineCase.Algorithm.World.Plants
                                 }
                             }
                         }
+                    }
                 }
             }
         }
