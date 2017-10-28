3566e2e30eb90aaf0efee4dbe384e700c26fce6d
WangJun <1103870790@qq.com>
2017-10-28 18:31:40 +0800

    Walking with terrain considered

--- src/MineCase.Server.Grains/Game/Entities/Components/EntityAiComponent.cs
+++ src/MineCase.Server.Grains/Game/Entities/Components/EntityAiComponent.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using MineCase.Algorithm;
 using MineCase.Algorithm.Game.Entity.Ai.MobAi;
 using MineCase.Engine;
+using MineCase.Graphics;
 using MineCase.Server.Components;
 using MineCase.Server.Game.BlockEntities;
 using MineCase.Server.World;
@@ -103,7 +105,7 @@ namespace MineCase.Server.Game.Entities.Components
             }
         }
 
-        private Task ActionWalk()
+        private async Task ActionWalk()
         {
             float step = 0.3f;
             float theta = (float)(random.NextDouble() * 2 * Math.PI);
@@ -114,18 +116,56 @@ namespace MineCase.Server.Game.Entities.Components
             {
                 head = theta;
 
-                AttachedObject.SetLocalValue(EntityLookComponent.YawProperty, (float)(head / Math.PI * 180.0f));
-                AttachedObject.SetLocalValue(EntityLookComponent.HeadYawProperty, (float)(head / Math.PI * 180.0f));
+                await AttachedObject.SetLocalValue(EntityLookComponent.YawProperty, (float)(head / Math.PI * 180.0f));
+                await AttachedObject.SetLocalValue(EntityLookComponent.HeadYawProperty, (float)(head / Math.PI * 180.0f));
             }
             else
             {
                 head = (float)(yaw / 180.0f * Math.PI);
             }
 
-            AttachedObject.SetLocalValue(
+            // 新的位置
+            EntityWorldPos entityPos = new EntityWorldPos(pos.X - step * (float)Math.Sin(head), pos.Y, pos.Z + step * (float)Math.Cos(head));
+            BlockWorldPos blockPos = entityPos.ToBlockWorldPos();
+
+            // 检测行进方向的方块是否满足要求
+            Cuboid entityBoundbox = new Cuboid(new Point3d(entityPos.X, entityPos.Y, entityPos.Z), new Size(1, 1, 2)); // TODO data from Boundbox component
+            var chunkAccessor = AttachedObject.GetComponent<ChunkAccessorComponent>();
+            bool isCollided = false;
+            int yJumpHeight = 0;
+
+            // 检测此位置会不会与方块碰撞
+            for (int i = 1; blockPos.Y + i < 256 && i <= 3; ++i)
+            {
+                BlockWorldPos upblock = BlockWorldPos.Add(blockPos, 0, i, 0);
+                BlockState upstate = await chunkAccessor.GetBlockState(upblock);
+                if (!upstate.IsAir())
+                {
+                    Cuboid blockBoundbox = new Cuboid(new Point3d(upblock.X, upblock.Y, upblock.Z), new Size(1, 1, 1));
+                    if (Collision.IsCollided(entityBoundbox, blockBoundbox))
+                    {
+                        isCollided = true;
+                        break;
+                    }
+                }
+            }
+
+            // 获得高度变化
+            for (int i = -2; blockPos.Y + i < 256 && i <= 0; ++i)
+            {
+                BlockState state = await chunkAccessor.GetBlockState(BlockWorldPos.Add(blockPos, 0, i, 0));
+                if (!state.IsAir())
+                {
+                    yJumpHeight = i + 1;
+                }
+            }
+
+            if (!isCollided)
+            {
+                await AttachedObject.SetLocalValue(
                     EntityWorldPositionComponent.EntityWorldPositionProperty,
-                    new EntityWorldPos(pos.X - step * (float)Math.Sin(head), pos.Y, pos.Z + step * (float)Math.Cos(head)));
-            return Task.CompletedTask;
+                    EntityWorldPos.Add(entityPos, 0, yJumpHeight, 0));
+            }
         }
 
         private Task ActionFollow()
@@ -140,7 +180,7 @@ namespace MineCase.Server.Game.Entities.Components
             return Task.CompletedTask;
         }
 
-        private Task OnGameTick(object sender, (TimeSpan deltaTime, long worldAge) e)
+        private async Task OnGameTick(object sender, (TimeSpan deltaTime, long worldAge) e)
         {
             /*
             if (e.worldAge % 16 == 0)
@@ -172,16 +212,16 @@ namespace MineCase.Server.Game.Entities.Components
             // random walk
             if (state == CreatureState.Stop && random.Next(10) == 0)
             {
-                AttachedObject.SetLocalValue(EntityAiComponent.CreatureEventProperty, CreatureEvent.RandomWalk);
+                await AttachedObject.SetLocalValue(EntityAiComponent.CreatureEventProperty, CreatureEvent.RandomWalk);
             }
             else if (state == CreatureState.Walk && random.Next(30) == 0)
             {
-                AttachedObject.SetLocalValue(EntityAiComponent.CreatureEventProperty, CreatureEvent.Stop);
+                await AttachedObject.SetLocalValue(EntityAiComponent.CreatureEventProperty, CreatureEvent.Stop);
             }
 
             CreatureEvent evnt = AttachedObject.GetValue(EntityAiComponent.CreatureEventProperty);
             CreatureState newState = ai.GetState(state, evnt);
-            AttachedObject.SetLocalValue(EntityAiComponent.CreatureStateProperty, newState);
+            await AttachedObject.SetLocalValue(EntityAiComponent.CreatureStateProperty, newState);
             switch (newState)
             {
                 case CreatureState.Attacking:
@@ -199,15 +239,13 @@ namespace MineCase.Server.Game.Entities.Components
                 case CreatureState.Follow:
                     break;
                 case CreatureState.Walk:
-                    ActionWalk();
+                    await ActionWalk();
                     break;
                 case CreatureState.Stop:
                     break;
                 default:
                     throw new NotSupportedException("Unsupported state.");
             }
-
-            return Task.CompletedTask;
         }
     }
 }
