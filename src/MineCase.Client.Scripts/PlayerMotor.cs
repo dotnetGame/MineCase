cdb998f2839101ce188887e6f86ef17f066a0538
SunnyCase <sunnycase@live.cn>
2017-11-06 18:44:06 +0800

    Add scripts project

---
+++ src/MineCase.Client.Scripts/PlayerMotor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MineCase.Client
+{
+    public class PlayerMotor : MonoBehaviour
+    {
+        private void Start()
+        {
+            Debug.Log("Hello!");
+        }
+    }
+}
---
+++ src/MineCase.Client/Assets/Scripts/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bootstrapper : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
