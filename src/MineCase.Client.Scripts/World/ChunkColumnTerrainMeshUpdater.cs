c0c08543731053af78dec152f7305fd73306951d
SunnyCase <sunnycase@live.cn>
2017-11-08 18:40:27 +0800

    Add reference to core

---
+++ src/MineCase.Client.Scripts/World/ChunkColumnTerrainMeshUpdater.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace MineCase.Client.World
+{
+    [RequireComponent(typeof(MeshFilter))]
+    public class ChunkColumnTerrainMeshUpdater : MonoBehaviour
+    {
+        private Mesh _chunkMesh;
+
+        private void Start()
+        {
+            _chunkMesh = new Mesh { name = "Chunk Column" };
+            GetComponent<MeshFilter>().mesh = _chunkMesh;
+        }
+
+        public void LoadFromChunkData(MineCase.World.ChunkColumnStorage storage)
+        {
+        }
+    }
+}
