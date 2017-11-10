3ff8840f1730e31552a067338f6a06cd9c17af17
SunnyCase <sunnycase@live.cn>
2017-11-10 10:08:39 +0800

    Add lost meta files

---
+++ src/MineCase.Client.Scripts/Network/ServerboundPacketSink.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MineCase.Protocol;
+
+namespace MineCase.Client.Network
+{
+    internal class ServerboundPacketSink : IPacketSink
+    {
+        private readonly IPacketPackager _packetPackager;
+
+        public ServerboundPacketSink(IPacketPackager packetPackager)
+        {
+            _packetPackager = packetPackager;
+        }
+
+        public async Task SendPacket(ISerializablePacket packet)
+        {
+            var prepared = _packetPackager.PreparePacket(packet);
+            await SendPacket(prepared.packetId, prepared.data);
+        }
+
+        public Task SendPacket(uint packetId, byte[] data)
+        {
+            var packet = new UncompressedPacket
+            {
+                PacketId = packetId,
+                Data = new ArraySegment<byte>(data)
+            };
+
+            return Task.CompletedTask;
+        }
+    }
+}
