6268d6b69e376609ada062f0fb939f51e5cd2c71
SunnyCase <sunnycase@live.cn>
2017-11-16 17:45:14 +0800

    update case

---
+++ src/MineCase.Core/Game/Entity/Ai/CreatureState.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Server.World.EntitySpawner
+{
+    public enum CreatureState : int
+    {
+        Stop,
+        Walk,
+        Look,
+        Follow,
+        EatingGrass,
+        BurnedBySunshine,
+        Burned,
+        Escaping,
+        Attacking,
+        Explosion
+    }
+}
