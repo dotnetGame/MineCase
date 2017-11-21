feb477eaaa0378176ae0ccd0b78d1c49dc544d34
WangJun <1103870790@qq.com>
2017-11-21 16:42:03 +0800

    Add creature animation

---
+++ src/MineCase.Algorithm/Game/Entity/Ai/Action/CreatureAnimation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Algorithm.Game.Entity.Ai.Action
+{
+    public abstract class CreatureAnimation
+    {
+        protected CreatureAction _begin;
+        protected CreatureAction _end;
+        protected int _time;
+        protected int _curtime;
+        protected CreatureActionDistance _step;
+
+        public CreatureAnimation(CreatureAction begin, CreatureAction end, int time)
+        {
+            _begin = begin;
+            _end = end;
+            _time = time;
+            _curtime = 0;
+            _step = (_end - _begin) / _time;
+        }
+
+        public abstract CreatureAction Step(int time);
+    }
+
+    public class CreatureAnimationLinear : CreatureAnimation
+    {
+        public CreatureAnimationLinear(CreatureAction begin, CreatureAction end, int time)
+            : base(begin, end, time)
+        {
+        }
+
+        public override CreatureAction Step(int time)
+        {
+            CreatureAction result = new CreatureAction();
+            _curtime += time;
+            result = _begin + _step * _curtime;
+            return result;
+        }
+    }
+}
