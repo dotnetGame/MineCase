7a7d9c890ae286161849f49721a1c6e2c0006b32
Melonpi <melonpi1943@gmail.com>
2017-12-12 15:59:35 +0800

    Build scripts (#112)
    * add 'checking if mongodb is online' in build scripts.
    
    * fix build script exit value.

--- build_and_run.bat
+++ build_and_run.bat
@@ -1,4 +1,17 @@
 @echo off
+where mongo
+IF %ERRORLEVEL% NEQ 0 (
+    echo "mongo command is not available, please make sure mongodb is installed and added to the system path."
+    exit /b 1
+)
+
+mongo --eval "db.stats()"
+IF %ERRORLEVEL% NEQ 0 (
+    echo "mongodb not running"
+    exit /b 1
+) else (
+    echo "mongodb is online..."
+)
 
 echo building MineCase...
 pushd src
