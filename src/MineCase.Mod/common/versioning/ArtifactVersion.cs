cc7662caf516369e9dace547c7d8e01ea1ae029d
JunWang <jstzwj@aliyun.com>
2018-01-26 16:04:53 +0800

    Add more mod structures

---
+++ src/MineCase.Mod/common/versioning/ArtifactVersion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Mod.common.versioning
+{
+    public interface ArtifactVersion : IComparable<ArtifactVersion>
+    {
+        string GetLabel();
+
+        string GetVersionString();
+
+        bool ContainsVersion(ArtifactVersion source);
+
+        string GetRangeString();
+    }
+}
