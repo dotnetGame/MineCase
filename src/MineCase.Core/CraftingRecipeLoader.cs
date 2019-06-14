24f8c6509d101357379c11f2ef169886f44e34ab
JasonWang <1103870790@qq.com>
2019-06-14 22:20:46 +0800

    Fix Craft recipe loader bug and add digging data

--- src/MineCase.Core/CraftingRecipeLoader.cs
+++ src/MineCase.Core/CraftingRecipeLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -61,29 +62,32 @@ namespace MineCase
                 recipe.Output.ItemCount = byte.Parse(ToString(resultSpan.Slice(resultSplitter + 1)));
             }
 
+            var recipeSlots = new List<CraftingRecipeSlot>();
             var restSpan = ingredientsSpan;
             while (!restSpan.IsEmpty)
             {
                 var ingredientSplitter = restSpan.IndexOf('|');
                 var ingredientSpan = ingredientSplitter == -1 ? restSpan : restSpan.Slice(0, ingredientSplitter);
-                ParseIngredient(ingredientSpan, recipe);
+                ParseIngredient(ingredientSpan, recipe, recipeSlots);
                 if (ingredientSplitter == -1)
                     break;
                 else
                     restSpan = restSpan.Slice(ingredientSplitter + 1);
             }
 
+            recipe.Inputs = recipeSlots.ToArray();
+            NormalizeIngredients(recipe);
+
             Recipes.Add(recipe);
         }
 
-        private void ParseIngredient(Span<char> ingredientSpan, CraftingRecipe recipe)
+        private void ParseIngredient(Span<char> ingredientSpan, CraftingRecipe recipe, List<CraftingRecipeSlot> recipeSlots)
         {
             var slot = new Slot { ItemCount = 1 };
             var splitter = ingredientSpan.IndexOf(',');
             ParseItem(ingredientSpan.Slice(0, splitter), ref slot);
             var distributionSpan = ingredientSpan.Slice(splitter + 1);
 
-            var recipeSlots = new List<CraftingRecipeSlot>();
             do
             {
                 var positionSplitter = distributionSpan.IndexOf(',');
@@ -131,8 +135,6 @@ namespace MineCase
                     distributionSpan = distributionSpan.Slice(positionSplitter + 1);
             }
             while (true);
-            recipe.Inputs = recipeSlots.ToArray();
-            NormalizeIngredients(recipe);
         }
 
         private void NormalizeIngredients(CraftingRecipe recipe)
---
+++ src/MineCase.Server.Interfaces/World/Decoration/Biomes/IBiomeSwampDecorator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Server.World.Decoration.Biomes
+{
+    public interface IBiomeSwampDecorator : IBiomeDecorator
+    {
+    }
+}
