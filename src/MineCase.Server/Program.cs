8d38cc49909c89f2b5ea202330a5f5b2eeec8c4b
JasonWang <1103870790@qq.com>
2019-08-22 02:43:29 +0800

    orleans hello world

--- src/MineCase.Server/Program.cs
+++ src/MineCase.Server/Program.cs
@@ -1,12 +1,62 @@
-﻿using System;
+﻿using Orleans;
+using Orleans.ApplicationParts;
+using Orleans.Configuration;
+using Orleans.Hosting;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace MineCase.Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            return RunMainAsync().Result;
+        }
+
+        private static async Task<int> RunMainAsync()
+        {
+            try
+            {
+                var host = await StartSilo();
+                Console.WriteLine("Press Enter to terminate...");
+                Console.ReadLine();
+
+                await host.StopAsync();
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return 1;
+            }
+        }
+
+        private static async Task<ISiloHost> StartSilo()
+        {
+            // define the cluster configuration
+            var builder = new SiloHostBuilder()
+                .UseLocalhostClustering()
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "dev";
+                    options.ServiceId = "MineCaseService";
+                })
+                .Configure<EndpointOptions>(options => options.AdvertisedIPAddress = IPAddress.Loopback)
+                .ConfigureApplicationParts(ConfigureApplicationParts)
+                .ConfigureLogging(logging => logging.AddConsole());
+
+            var host = builder.Build();
+            await host.StartAsync();
+            return host;
+        }
+
+        private static void ConfigureApplicationParts(IApplicationPartManager parts)
+        {
+            parts.AddFromApplicationBaseDirectory().WithReferences();
         }
     }
 }
