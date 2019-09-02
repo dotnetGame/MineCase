5cf480f485cf1dfaee996c0e141c3ba306ed11b9
JasonWang <1103870790@qq.com>
2019-09-02 12:21:13 +0800

    pack packet

--- src/MineCase.Gateway/Network/ClientboundPacketSink.cs
+++ src/MineCase.Gateway/Network/ClientboundPacketSink.cs
@@ -1,35 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using MineCase.Protocol;
+using Orleans.Concurrency;
 
 namespace MineCase.Gateway.Network
 {
     public class ClientboundPacketSink
     {
-        public ClientboundPacketSink(IPacketPackager packetPackager)
+        private PacketPackager _packetPackager;
+
+        public ClientboundPacketSink(PacketPackager packetPackager)
         {
             _packetPackager = packetPackager;
-        }
-
-        public override Task OnActivateAsync()
-        {
-            _subsManager = new Grains.GrainObserverManager<IClientboundPacketObserver>();
-            _subsManager.ExpirationDuration = new TimeSpan(0, 0, 20);
-            return base.OnActivateAsync();
-        }
-
-        // Clients call this to subscribe.
-        public Task Subscribe(IClientboundPacketObserver observer)
-        {
-            _subsManager.Subscribe(observer);
-            return Task.CompletedTask;
-        }
-
-        // Also clients use this to unsubscribe themselves to no longer receive the messages.
-        public Task UnSubscribe(IClientboundPacketObserver observer)
-        {
-            _subsManager.Unsubscribe(observer);
-            return Task.CompletedTask;
         }
 
         public async Task SendPacket(ISerializablePacket packet)
@@ -43,26 +27,8 @@ namespace MineCase.Gateway.Network
             var packet = new UncompressedPacket
             {
                 PacketId = packetId,
-                Data = new ArraySegment<byte>(data.Value)
+                Data = new byte[data.Value.Length]
             };
-            if (_subsManager.Count == 0)
-                DeactivateOnIdle();
-            else
-                _subsManager.Notify(n => n.ReceivePacket(packet));
-            return Task.CompletedTask;
-        }
-
-        public Task Close()
-        {
-            _subsManager.Notify(n => n.OnClosed());
-            _subsManager.Clear();
-            DeactivateOnIdle();
-            return Task.CompletedTask;
-        }
-
-        public Task NotifyUseCompression(uint threshold)
-        {
-            _subsManager.Notify(n => n.UseCompression(threshold));
             return Task.CompletedTask;
         }
     }
