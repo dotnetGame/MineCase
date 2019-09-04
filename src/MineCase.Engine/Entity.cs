c94dd680536142a0475828198f504bbd34920937
JasonWang <1103870790@qq.com>
2019-09-05 01:46:06 +0800

    add systems

--- src/MineCase.Engine/Entity.cs
+++ src/MineCase.Engine/Entity.cs
@@ -8,5 +8,52 @@ namespace MineCase.Engine
     {
         private Guid _entityId;
         private List<IComponent> _components;
+        private Dictionary<string, int> _nameIndex;
+
+        public Entity()
+        {
+            _entityId = new Guid();
+            _components = new List<IComponent>();
+            _nameIndex = new Dictionary<string, int>();
+        }
+
+        public void AddComponent(IComponent component)
+        {
+            var name = component.GetType().Name;
+            if (_nameIndex.ContainsKey(name))
+                throw new Exception("The component has been added to the entity");
+
+            _components.Add(component);
+            _nameIndex.Add(name, _components.Count - 1);
+        }
+
+        public void RemoveComponent<T>()
+        {
+            var name = typeof(T).Name;
+            if (!_nameIndex.ContainsKey(name))
+                throw new Exception("The entity does not have this component");
+            int index = _nameIndex[name];
+            _nameIndex.Remove(name);
+            _components.RemoveAt(index);
+        }
+
+        public T GetComponent<T>()
+        {
+            var name = typeof(T).Name;
+            if (!_nameIndex.ContainsKey(name))
+                throw new Exception("The entity does not have this component");
+            int index = _nameIndex[name];
+            return (T)_components[index];
+        }
+
+        public void SetComponent(IComponent component)
+        {
+            var name = component.GetType().Name;
+            if (!_nameIndex.ContainsKey(name))
+                throw new Exception("The entity does not have this component");
+            int index = _nameIndex[name];
+            _components[index] = component;
+        }
+
     }
 }
---
+++ src/MineCase.Engine/ITearDownSystem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MineCase.Engine
+{
+    public interface ITearDownSystem : ISystem
+    {
+        void TearDown();
+    }
+}
