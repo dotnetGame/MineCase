22f608054d6c943ef0c3049b868dfa27b16986e1
JasonWang <1103870790@qq.com>
2020-06-08 16:33:30 +0800

    update doc

---
+++ src/MineCase.Server.Grains/Game/Entities/Components/ChatComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MineCase.Engine;
+
+namespace MineCase.Server.Game.Entities.Components
+{
+    internal class ChatComponent : Component<PlayerGrain>
+    {
+        public ChatComponent(string name = "chat")
+            : base(name)
+        {
+        }
+    }
+}
